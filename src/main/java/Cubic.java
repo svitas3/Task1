c0c24ca66b0554029f739725da0086a062576c51
Svit.as <lyapinasv@edu.spbstu.ru>
2023-06-08 00:15:31 +0300

    -

--- src/main/java/Cubic.java
+++ src/main/java/Cubic.java
@@ -1,2 +1,117 @@
-package PACKAGE_NAME;public class Cubic {
+public class Cubic {
+    private int[][][] cube;
+    private int size;
+
+    public Cubic(int size) {
+        this.size = size;
+        this.cube = new int[size][size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    this.cube[i][j][k] = i * size * size + j * size + k + 1;
+                }
+            }
+        }
+    }
+
+    // поворот указанного числа слоев
+    public void rotate(int layer, boolean clockwise) {
+        int[][] temp = new int[size][size];
+        int index = clockwise ? size - layer : layer - 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (index >= 0 && index <= size - 1) {
+                    if (clockwise) {
+                        temp[j][size - index - 1] = cube[index][j][layer - 1];
+                    } else {
+                        temp[size - j - 1][index] = cube[index][j][size - layer];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (index >= 0 && index <= size - 1) {
+                    if (clockwise) {
+                        cube[index][j][layer - 1] = temp[i][j];
+                    } else {
+                        cube[index][j][size - layer] = temp[size - j - 1][i];
+                    }
+                }
+            }
+        }
+    }
+
+    // поворот всего кубика
+    public void shuffle() {
+        int[][][] temp = new int[size][size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    temp[i][j][k] = cube[size - j - 1][i][k];
+                }
+            }
+        }
+        cube = temp;
+    }
+
+    // запрос состояния грани
+    public int[][] getFace(int face) {
+        int[][] result = new int[size][size];
+        switch (face) {
+            case 1:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[0][i][j];
+                    }
+                }
+                break;
+            case 2:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[size - 1][i][j];
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[i][0][j];
+                    }
+                }
+                break;
+            case 4:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[i][size - 1][j];
+                    }
+                }
+                break;
+            case 5:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[i][j][0];
+                    }
+                }
+                break;
+            case 6:
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
+                        result[i][j] = cube[i][j][size - 1];
+                    }
+                }
+                break;
+            default:
+                System.out.println("Incorrect face number.");
+        }
+        return result;
+    }
+
+    // случайная установка состояния кубика
+    public void randomize() {
+        for (int i = 0; i < size; i++) {
+            this.rotate(i + 1, Math.random() < 0.5);
+        }
+    }
 }
--- src/test/java/CubicTest.java
+++ src/test/java/CubicTest.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class CubicTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CubicTest {
+    @Test
+    public void testRotateClockwise() {
+        Cubic cube = new Cubic(2); // создаем кубик 2x2
+        cube.rotate(1, true); // поворачиваем первый слой по часовой стрелке
+        int[][] face = cube.getFace(1); // получаем первую грань кубика
+        // проверяем, что состояние грани изменилось корректно после поворота
+        assertArrayEquals(new int[][] {{1, 2}, {3, 4}}, face);
+    }
+
+    @Test
+    public void testRotateCounterclockwise() {
+        Cubic cube = new Cubic(2); // создаем кубик 2x2
+        cube.randomize(); // перемешиваем кубик случайным образом
+        int[][] faceBefore = cube.getFace(1); // получаем состояние первой грани перед поворотом
+        cube.rotate(2, false); // поворачиваем второй слой против часовой стрелки
+        int[][] faceAfter = cube.getFace(1); // получаем состояние первой грани после поворота
+        // проверяем, что состояние грани изменилось корректно после поворота
+        assertNotEquals(faceBefore, faceAfter);
+    }
+
+    @Test
+    public void testShuffle() {
+        Cubic cube = new Cubic(3); // создаем кубик 3x3
+        cube.shuffle(); // перемешиваем кубик
+        int[][] face = cube.getFace(1); // получаем первую грань кубика
+        // проверяем, что все элементы грани находятся в диапазоне от 1 до 9
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertTrue(face[i][j] >= 1 && face[i][j] <= 9);
+            }
+        }
+    }
+
+    @Test
+    public void testRandomize() {
+        Cubic cube = new Cubic(2); // создаем кубик 2x2
+        int[][] faceBefore = cube.getFace(4); // получаем состояние четвертой грани перед перемешиванием
+        cube.randomize(); // перемешиваем кубик
+        int[][] faceAfter = cube.getFace(4); // получаем состояние четвертой грани после перемешивания
+        // проверяем, что состояние грани изменилось корректно после перемешивания
+        assertNotEquals(faceBefore, faceAfter);
+    }
 }
+
+//В данном примере представлены четыре теста. Первый тест проверяет, что возможно повернуть один слой кубика по часовой стрелке. Второй тест проверяет, что возможно повернуть один слой кубика против часовой стрелки. Третий тест проверяет, что кубик может быть перемешан случайным образом. Четвертый тест проверяет, что кубик может быть перемешан с помощью метода randomize(). Общее количество тестов можно увеличить, проверив дополнительные краевые случаи или используя более сложные алгоритмы поворота кубика.
